3c9a7582cac258a45e57ee98750ec3203e8e5584
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2012-05-24 00:59:37 -0400

    Cleaned up disk reporting

--- sysinfo.sh
+++ sysinfo.sh
@@ -16,13 +16,13 @@
 #
 # https://github.com/ScottSteiner/shell-scripts
 
-cpumodel=`grep -m 1 'model name' /proc/cpuinfo | sed 's/.*: //'`
-cpuspeed=`awk '/cpu MHz/ { print $4; exit }' /proc/cpuinfo | sed 's/\.[0-9][0-9][0-9]//' `
+cpuModel=`awk -F ': ' '/model name/ { print $2; exit }' /proc/cpuinfo | sed -e 's/(tm)//g;s/Dual Core Processor //g'`
+cpuSpeed=`awk '/cpu MHz/ { print $4; exit }' /proc/cpuinfo | sed 's/\.[0-9]*//' `
 #cpuusage=`mpstat 1 1| awk '/all/ { print 100-$12; exit }'`
 #Show every partition.  Really spammy:
 #disks=`df -h | awk '/^\/dev/ { print $6 ": " $3 "/" $2 " (" $5 ")"; exit}' | awk '{ str1=str1 $0 " "}END{ print str1 }'`
 #Show only the totals:
-disks=`df -lhx tmpfs --total|awk '/^total/ { print $3"/"$2" ("$5")"; exit }'`
+disks=`df -lhx devtmpfs -x tmpfs -x rootfs --total|awk '/^total/ { print $3"/"$2" ("$5")"; exit }'`
 
 if [ -e /etc/issue ] && [[ `grep -i Arch /etc/issue` == *Arch* ]];then os='Arch GNU/Linux'
 elif [ -e /etc/issue.net ]; then os=`cat /etc/issue.net`
@@ -32,19 +32,19 @@ elif [ `uname -o` == 'GNU/Linux' ]; then os='Unknown GNU/Linux'
 else os='N/A'
 fi
 hostname=`uname -n`
-kernelname=`uname -s`
-kernelversion=`uname -r`
-#memcur=`free -mo | awk '/Mem:/ { print $3; exit }'` #Includes cached memory (in megabytes)
-memcur=`free -m | awk '/cache:/ { print $3; exit }'` #Doesn't include cached memory (in megabytes)
-memmax=`free -mo | awk '/Mem:/ { print $2; exit }'`
-mempercent=`echo 'scale=2;100*'$memcur'/'$memmax | bc`
+kernelName=`uname -s`
+kernelVersion=`uname -r`
+#memoryCurrent=`free -mo | awk '/Mem:/ { print $3; exit }'` #Includes cached memory (in megabytes)
+memoryCurrent=`free -m | awk '/cache:/ { print $3; exit }'` #Doesn't include cached memory (in megabytes)
+memoryMax=`free -mo | awk '/Mem:/ { print $2; exit }'`
+memoryPercent=`echo 'scale=2;100*'$memoryCurrent'/'$memoryMax | bc`
 #nettransfer=`ifconfig eth0 | awk '/RX bytes/ { print "Rec: "$3$4" Sent: " $7$8; exit }' | sed -e 's/(//g;s/)//g'`
-proccount=$((`ps -e --noheading|wc -l`-1))
-swapcur=`free -mo | awk '/Swap:/ { print $3; exit }'`
-swapmax=`free -mo | awk '/Swap:/ { print $2; exit }'`
-if [ $swapmax -gt 0 ]; then swappercent=`echo 'scale=2;100*'$swapcur'/'$swapmax | bc`
-else swappercent='100'
+procCount=$((`ps -e --noheading|wc -l`-1))
+swapCurrent=`free -mo | awk '/Swap:/ { print $3; exit }'`
+swapMax=`free -mo | awk '/Swap:/ { print $2; exit }'`
+if [ $swapMax -gt 0 ]; then swapPercent=`echo 'scale=2;100*'$swapCurrent'/'$swapMax | bc`
+else swapPercent='100'
 fi
 uptime=`uptime|awk -F 'up ' '{ print $2; exit }'|sed 's/,..[0-9] user.*$//'`
-video=`lspci -v | grep -i 'VGA compatible controller' | sed -e 's/^.*controller: //g;s/ (prog.*$//g;s/ Technologies Inc//g'`
-echo "Hostname: $hostname OS: $os Kernel: $kernelname $kernelversion Up: $uptime CPU: $cpumodel "$cpuspeed"MHz Video: $video Processes: $proccount RAM: "$memcur"mb/"$memmax"mb ("$mempercent"%) Swap: "$swapcur"mb/"$swapmax"mb ("$swappercent"%) Disks: $disks $nettransfer"|sed  's/  / /g'
+video=`lspci -v | grep -i 'VGA compatible controller' | sed -e 's/^.*controller: //g;s/ (prog.*$//g;s/ Technologies Inc//g;s/Advanced Micro Devices \[AMD\] nee //g'`
+echo "Hostname: $hostname OS: $os Kernel: $kernelName $kernelVersion Up: $uptime CPU: $cpuModel ${cpuSpeed}MHz Video: $video Processes: $procCount RAM: ${memoryCurrent}mb/${memoryMax}mb (${memoryPercent}%) Swap: ${swapCurrent}mb/${swapMax}mb (${swapPercent}%) Disks: $disks $nettransfer"|sed  's/  / /g'
