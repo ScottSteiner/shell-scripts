35f008f6bdd0d567177a1ae32020be91ebc5f679
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2013-02-18 04:09:55 -0500

    Added support for /q/ and /wsg/

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2009-2012 ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
+# Copyright (c) 2009-2013 ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ board=$1
 thread=$2
 savepath=/www/threads/
 case "$board" in
-	3|a|adv|an|b|c|cgl|ck|cm|co|d|diy|e|f|fa|fit|g|gif|h|hc|hm|hr|i|ic|int|jp|k|lit|m|mlp|mu|n|new|o|p|po|pol|r|r9k|s|sci|soc|sp|t|tg|toy|trv|tv|u|v|vg|vp|w|wg|x|y)
+	3|a|adv|an|b|c|cgl|ck|cm|co|d|diy|e|f|fa|fit|g|gif|h|hc|hm|hr|i|ic|int|jp|k|lit|m|mlp|mu|n|new|o|p|po|pol|q|r|r9k|s|sci|soc|sp|t|tg|toy|trv|tv|u|v|vg|vp|w|wg|wsg|x|y)
 		wget --execute robots=off --recursive --convert-links --page-requisites --no-directories --span-hosts --domains=images.4chan.org,static.4chan.org,thumbs.4chan.org --mirror \
 			--quiet --directory-prefix=$savepath$board-$thread --include-directories=*/src,image,*/thumb http://boards.4chan.org/$board/res/$thread
 		mv $savepath$board-$thread/$thread $savepath$board-$thread/index.html
